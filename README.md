5c9017d14b6225abe882f64641c650f623051545
neil.jia <neil.jia@elitesland.com>
2017-07-26 00:57:52 +0800

    bulid时生成war文件
    将生成文件由默认的jar改为war，以便部署至tomcat

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ##参考资料
 - [目录](http://blog.csdn.net/column/details/13987.html)
 - [初识Spring Boot框架](http://blog.csdn.net/u012702547/article/details/53740047)
+##如何构建war文件(SpringBoot默认为jar文件)
+- [SpringBoot使用Gradle构建war包的步骤](http://blog.csdn.net/KingBoyWorld/article/details/75244769)
+- [官方文档:Spring Boot Reference Guide: 67.3 Packaging executable jar and war files](http://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#build-tool-plugins-gradle-packaging)
+- [官方文档:Spring Boot Reference Guide: 85.1 Create a deployable war file](http://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#howto-create-a-deployable-war-file)
--- src/main/java/com/mytest/MySpringBootDemoApplication.java
+++ src/main/java/com/mytest/MySpringBootDemoApplication.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class MySpringBootDemoApplication {
+public class MySpringBootDemoApplication extends SpringBootServletInitializer {
 
 	@Value(value = "${app.name}")
 	private String appNme;
@@ -17,13 +19,22 @@ public class MySpringBootDemoApplication {
 	@Autowired
 	private Book book;
 
+	/**
+	 *重写configure,用于生成war文件时,能够成功初始化Servlet
+	 * @param application
+	 * @return
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MySpringBootDemoApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(MySpringBootDemoApplication.class, args);
 	}
 
 	@RequestMapping(value = "/",produces = "text/plain;charset=UTF-8")
 	String index(){
-//		return "Hello, This is Spring Boot Demo!";
-		return "Hello Spring Boot! App_Name="+appNme+" The BookName is "+book.getName()+";and Book Author is "+book.getAuthor()+";and Book price is "+book.getPrice();
+		return "Hello, Spring Boot! App_Name="+appNme+" The BookName is "+book.getName()+";and Book Author is "+book.getAuthor()+";and Book price is "+book.getPrice();
 	}
 }
